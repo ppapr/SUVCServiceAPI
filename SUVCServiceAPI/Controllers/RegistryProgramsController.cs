42c2428aad25099c004b93147a4c37c2b6449e68
putilovpavel <putilovpavel@K21504N13>
2023-11-18 10:33:48 +0500

    Add response and model for RegistryPrograms

---
+++ SUVCServiceAPI/Controllers/RegistryProgramsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SUVCServiceAPI.Entities;
+using SUVCServiceAPI.Models;
+
+namespace SUVCServiceAPI.Controllers
+{
+    public class RegistryProgramsController : ApiController
+    {
+        private SUVCServiceDbEntities db = new SUVCServiceDbEntities();
+
+        // GET: api/RegistryPrograms
+        [ResponseType(typeof(List<ResponseRegistry>))]
+        public IHttpActionResult GetRegistryPrograms()
+        {
+            return Ok(db.RegistryPrograms.ToList().ConvertAll(p => new ResponseRegistry(p)));
+        }
+
+        // GET: api/RegistryPrograms/5
+        [ResponseType(typeof(RegistryPrograms))]
+        public IHttpActionResult GetRegistryPrograms(int id)
+        {
+            RegistryPrograms registryPrograms = db.RegistryPrograms.Find(id);
+            if (registryPrograms == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(registryPrograms);
+        }
+
+        // PUT: api/RegistryPrograms/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRegistryPrograms(int id, RegistryPrograms registryPrograms)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != registryPrograms.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(registryPrograms).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegistryProgramsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/RegistryPrograms
+        [ResponseType(typeof(RegistryPrograms))]
+        public IHttpActionResult PostRegistryPrograms(RegistryPrograms registryPrograms)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.RegistryPrograms.Add(registryPrograms);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = registryPrograms.ID }, registryPrograms);
+        }
+
+        // DELETE: api/RegistryPrograms/5
+        [ResponseType(typeof(RegistryPrograms))]
+        public IHttpActionResult DeleteRegistryPrograms(int id)
+        {
+            RegistryPrograms registryPrograms = db.RegistryPrograms.Find(id);
+            if (registryPrograms == null)
+            {
+                return NotFound();
+            }
+
+            db.RegistryPrograms.Remove(registryPrograms);
+            db.SaveChanges();
+
+            return Ok(registryPrograms);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RegistryProgramsExists(int id)
+        {
+            return db.RegistryPrograms.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ SUVCServiceAPI/Models/ResponseRegistry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SUVCServiceAPI.Entities;
+
+namespace SUVCServiceAPI.Models
+{
+    public class ResponseRegistry
+    {
+        public ResponseRegistry(RegistryPrograms registry)
+        {
+            ID = registry.ID;
+            NameProgram = registry.NameProgram;
+            VersionProgram  = registry.VersionProgram;
+            DescriptionProgram = registry.DescriptionProgram;
+            Specialization = registry.Specialization.NameSpecialization;
+        }
+        public int ID { get; set; }
+        public string NameProgram { get; set; }
+        public string VersionProgram { get; set; }
+        public string DescriptionProgram { get; set; }
+        public string Specialization { get; set; }
+    }
+}
