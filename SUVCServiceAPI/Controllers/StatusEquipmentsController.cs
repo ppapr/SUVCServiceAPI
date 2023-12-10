75d5b11fce1be41a86d0bbb6d3b97e662a0c1672
Pavel <Pavel@DESKTOP-PUIS5V4>
2023-12-10 14:42:47 +0500

    Add controller and model StatusEquipment

---
+++ SUVCServiceAPI/Controllers/StatusEquipmentsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SUVCServiceAPI.Entities;
+using SUVCServiceAPI.Models;
+
+namespace SUVCServiceAPI.Controllers
+{
+    public class StatusEquipmentsController : ApiController
+    {
+        private SUVCServiceDbEntities db = new SUVCServiceDbEntities();
+
+        // GET: api/StatusEquipments
+        [ResponseType(typeof(List<ResponseStatusEquipment>))]
+        public IHttpActionResult GetStatusEquipment()
+        {
+            return Ok(db.StatusEquipment.ToList().ConvertAll(p => new ResponseStatusEquipment(p)));
+        }
+
+        // GET: api/StatusEquipments/5
+        [ResponseType(typeof(StatusEquipment))]
+        public IHttpActionResult GetStatusEquipment(int id)
+        {
+            StatusEquipment statusEquipment = db.StatusEquipment.Find(id);
+            if (statusEquipment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(statusEquipment);
+        }
+
+        // PUT: api/StatusEquipments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStatusEquipment(int id, StatusEquipment statusEquipment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != statusEquipment.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(statusEquipment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StatusEquipmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/StatusEquipments
+        [ResponseType(typeof(StatusEquipment))]
+        public IHttpActionResult PostStatusEquipment(StatusEquipment statusEquipment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.StatusEquipment.Add(statusEquipment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = statusEquipment.ID }, statusEquipment);
+        }
+
+        // DELETE: api/StatusEquipments/5
+        [ResponseType(typeof(StatusEquipment))]
+        public IHttpActionResult DeleteStatusEquipment(int id)
+        {
+            StatusEquipment statusEquipment = db.StatusEquipment.Find(id);
+            if (statusEquipment == null)
+            {
+                return NotFound();
+            }
+
+            db.StatusEquipment.Remove(statusEquipment);
+            db.SaveChanges();
+
+            return Ok(statusEquipment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StatusEquipmentExists(int id)
+        {
+            return db.StatusEquipment.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ SUVCServiceAPI/Models/ResponseStatusEquipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SUVCServiceAPI.Entities;
+
+namespace SUVCServiceAPI.Models
+{
+    public class ResponseStatusEquipment
+    {
+        public ResponseStatusEquipment(StatusEquipment statusEquipment)
+        {
+            ID = statusEquipment.ID;
+            NameStatus = statusEquipment.NameStatus;
+        }
+
+        public int ID { get; set; }
+        public string NameStatus { get; set; }
+    }
+}
